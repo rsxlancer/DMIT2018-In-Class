c57af506a785ec9796041b8bef72b8e853879bfa
rsxlancer <rsxlancer@gmail.com>
2014-11-18 15:00:48 -0700

    Created Security Webpage

---
+++ eRestaurant sample/eRestaurant/BLL/Security/RoleManager.cs
@@ -0,0 +1,36 @@
+﻿using eRestaurant.DAL.Security;
+using eRestaurant.Entities.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.BLL.Security
+{
+    public class RoleManager : RoleManager<IdentityRole>
+    {
+        #region Constructors
+        public RoleManager()
+            : base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
+        {
+        }
+        #endregion
+
+        #region Methods
+        public void AddDefaultRoles()
+        {
+            foreach (string roleName in SecurityRoles.DefaultSecurityRoles)
+            {
+                // Check if it exists
+                if (!Roles.Any(r => r.Name == roleName))
+                {
+                    this.Create(new IdentityRole(roleName));
+                }
+            }
+        }
+        #endregion
+    }
+}
